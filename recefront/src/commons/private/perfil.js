29670b5bc3f618918ebab242c4b15350521d471e
CourtoisDiego <dfrivera026@gmail.com>
2020-11-29 02:14:33 -0600

    Usuario listo

---
+++ recefront/src/commons/private/perfil.js
@@ -0,0 +1,23 @@
+import './perfil.css'
+import Gordon from '../public/img/gordon.jpg'
+
+const Perfil = ()=>{
+    const usuario = [{"_id":1,"Nombre":"Cristiano Ronaldo","Usuario":"CR7","Pais":"Portugal"}];
+    const usuarioView = usuario.map((o)=>{
+        return (<li key={o._id}><div clasName="pp"><img src={Gordon}/></div>
+        <div className="Nombre">{o.Nombre}</div>
+        <div className="Usuario">{o.Usuario}</div>
+
+        <div className="Pais">{o.Pais}</div>
+
+        </li>);
+        })
+   return (
+       
+        <ul className="viewPerfil">
+        {usuarioView}
+        </ul>         
+   );
+}
+
+export default Perfil;
--- recefront/src/commons/private/usuario.js
+++ recefront/src/commons/private/usuario.js
@@ -1,8 +1,10 @@
 import Page from '../commons/page'
 import GetRecetas from '../private/getRecetas'
+import Perfil from '../private/perfil'
 const Usuario = ()=>{
     return (
         <Page headding="Usuario" footer="true">
+            <Perfil></Perfil>
             <GetRecetas></GetRecetas>
             
         </Page>
